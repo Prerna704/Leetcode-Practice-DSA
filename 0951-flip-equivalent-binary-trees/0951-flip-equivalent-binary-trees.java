6844492be5a1bad30c1b9519d7b14b1333615f47
PRERNA PREM <prernaprem720@gmail.com>
2026-05-22 14:32:09 +0530

    Time: 0 ms (100%), Space: 43 MB (77.89%) - LeetHub

--- 0951-flip-equivalent-binary-trees/0951-flip-equivalent-binary-trees.java
+++ 0951-flip-equivalent-binary-trees/0951-flip-equivalent-binary-trees.java
@@ -5,8 +5,11 @@ class Solution {
 
     if (node1 == null || node2 == null || node1.val != node2.val)
       return false;
-
-    return (flipEquiv(node1.left, node2.left) && flipEquiv(node1.right, node2.right)) ||
+      return (flipEquiv(node1.left, node2.left) && flipEquiv(node1.right, node2.right)) ||
         (flipEquiv(node1.left, node2.right) && flipEquiv(node1.right, node2.left));
+// there is two cases root is flipped and root is no tflipped jab root is not flipped to roo1.left == root2.left but when root is flipped root1.left == root2.right
+
+
+    
   }
 }
