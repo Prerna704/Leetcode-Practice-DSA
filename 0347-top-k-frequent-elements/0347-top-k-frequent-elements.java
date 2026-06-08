4a4422490d7cf9c36cd5e4d5458d197cd3d8bebd
PRERNA PREM <prernaprem720@gmail.com>
2026-06-08 18:21:25 +0530

    Time: 15 ms (55.04%), Space: 47.7 MB (31.44%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -2,7 +2,7 @@ class Solution {
     public int[] topKFrequent(int[] nums, int k) {
        HashMap<Integer,Integer> map = new HashMap<>();
        for(int num : nums){
-        map.put(num,map.getOrDefault(num,0) + 1);
+        map.put(num, map.getOrDefault(num,0) + 1);
        }
 
        PriorityQueue<Integer> pq = new PriorityQueue<>((a,b)->map.get(b) - map.get(a));
