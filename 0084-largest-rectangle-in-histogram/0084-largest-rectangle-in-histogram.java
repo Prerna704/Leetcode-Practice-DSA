6d5ba436fdf3b92db2e96744850ccca578ea89c8
PRERNA PREM <prernaprem720@gmail.com>
2026-06-30 15:37:51 +0530

    Time: 109 ms (18.03%), Space: 77.7 MB (68.54%) - LeetHub

---
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int largestRectangleArea(int[] heights) {
+         int n = heights.length;
+        int[] pse = new int[n];
+        int[] nse = new int[n];
+        Stack<Integer> st = new Stack<>();
+
+        for (int i = n - 1; i >= 0; i--) {
+            while (!st.isEmpty() && heights[i] <= heights[st.peek()])
+                st.pop();
+
+            nse[i] = st.isEmpty() ? n : st.peek();
+            st.push(i);
+        }
+
+        st.clear();
+
+        for (int i = 0; i < n; i++) {
+            while (!st.isEmpty() && heights[i] <= heights[st.peek()])
+                st.pop();
+
+            pse[i] = st.isEmpty() ? -1 : st.peek();
+            st.push(i);
+        }
+
+        int ans = 0;
+
+        for (int i = 0; i < n; i++) {
+            int area = (nse[i] - pse[i] - 1) * heights[i];
+            ans = Math.max(ans, area);
+        }
+
+        return ans;
+
+    }
+}
