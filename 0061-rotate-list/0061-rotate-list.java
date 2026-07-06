9cd21f9d576107bbf73c95b7161175175fc79861
PRERNA PREM <prernaprem720@gmail.com>
2026-07-06 15:11:37 +0530

    Time: 0 ms (100%), Space: 44.1 MB (80.71%) - LeetHub

--- 0061-rotate-list/0061-rotate-list.java
+++ 0061-rotate-list/0061-rotate-list.java
@@ -12,7 +12,7 @@ class Solution {
     public ListNode rotateRight(ListNode head, int k) {
          if (head == null || head.next == null || k == 0) return head;
 
-        // Step 1: calculate length
+        
         ListNode tail = head;
         int len = 1;
         while (tail.next != null) {
@@ -20,14 +20,13 @@ class Solution {
             len++;
         }
 
-        // Step 2: k modulo len
+        
         k = k % len;
         if (k == 0) return head;
 
-        // Step 3: make it circular
+       
         tail.next = head;
 
-        // Step 4: find new tail
         ListNode newTail = head;
         for (int i = 0; i < len - k - 1; i++) {
             newTail = newTail.next;
