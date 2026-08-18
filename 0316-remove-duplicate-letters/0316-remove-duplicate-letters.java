1daf6a6e94a02a7254436340d216bd302d1ab5b9
PRERNA PREM <prernaprem720@gmail.com>
2026-08-19 00:43:07 +0530

    Time: 12 ms (8.44%), Space: 46.5 MB (6.11%) - LeetHub

--- 0316-remove-duplicate-letters/0316-remove-duplicate-letters.java
+++ 0316-remove-duplicate-letters/0316-remove-duplicate-letters.java
@@ -1,26 +1,13 @@
 class Solution {
     public String removeDuplicateLetters(String s) {
-         int[] lastIndex = new int[26];
-        for (int i = 0; i < s.length(); i++){
-            lastIndex[s.charAt(i) - 'a'] = i; // track the lastIndex of character presence
-        }
-        
-        boolean[] seen = new boolean[26]; // keep track seen
-        Stack<Integer> st = new Stack();
-        
+          int[] cnt = new int[26];
+        int pos = 0; // the position for the smallest s[i]
+        for (int i = 0; i < s.length(); i++) cnt[s.charAt(i) - 'a']++;
         for (int i = 0; i < s.length(); i++) {
-            int curr = s.charAt(i) - 'a';
-            if (seen[curr]) continue; // if seen continue as we need to pick one char only
-            while (!st.isEmpty() && st.peek() > curr && i < lastIndex[st.peek()]){
-                seen[st.pop()] = false; // pop out and mark unseen
-            }
-            st.push(curr); // add into stack
-            seen[curr] = true; // mark seen
+            if (s.charAt(i) < s.charAt(pos)) pos = i;
+            if (--cnt[s.charAt(i) - 'a'] == 0) break;
         }
-
-        StringBuilder sb = new StringBuilder();
-        while (!st.isEmpty())
-            sb.append((char) (st.pop() + 'a'));
-        return sb.reverse().toString();
+        return s.length() == 0 ? "" : s.charAt(pos) + removeDuplicateLetters(s.substring(pos + 1).replaceAll("" + s.charAt(pos), ""));
     }
+    
 }
