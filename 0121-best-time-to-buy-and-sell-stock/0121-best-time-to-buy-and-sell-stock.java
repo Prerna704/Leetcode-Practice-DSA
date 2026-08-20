3774d4f9c675f5bade8ddd28794158bf9453e1a6
PRERNA PREM <prernaprem720@gmail.com>
2026-08-20 20:20:42 +0530

    Time: 1 ms (99.96%), Space: 94.6 MB (33.43%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -1,14 +1,18 @@
 class Solution {
-    public int maxProfit(int[] prices) {
-        int buy = prices[0];
+    public int maxProfit(int[] arr) {
+        int buy = arr[0];
         int profit = 0;
-        for(int i = 0; i < prices.length;i++){
-            if(prices[i] < buy){
-                buy = prices[i];
-            }else if(prices[i] - buy > profit){
-                profit = prices[i] - buy;
-            }
+        int n = arr.length;
+        
+        for(int i = 0; i < n; i++){
+                if(arr[i] < buy){
+                    buy = arr[i];
+                    
+                }else if(arr[i] - buy > profit){
+                    profit = arr[i] - buy;
+                }
         }
         return profit;
+
     }
 }
