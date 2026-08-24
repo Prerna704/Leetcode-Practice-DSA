a318dbe0f8037cdcc65719bd264f8fc3a07b42a1
PRERNA PREM <prernaprem720@gmail.com>
2026-08-24 20:19:30 +0530

    Time: 2 ms (91.96%), Space: 47.7 MB (57.67%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,17 +1,16 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-       int j = 0;
-       int n = nums.length;
-       for(int i = 0; i < n; i++){
-            if(nums[i] != 0){
-                nums[j] = nums[i];
-                j++;
-            }
-       }
-       while(j < n){
+        int j =0;
+      for(int i = 0; i <nums.length;i++){
+        if(nums[i] != 0){
+            nums[j] = nums[i];
+            j++;
+        }
+      }
+      while(j < nums.length){
         nums[j] = 0;
         j++;
+      }
        
-       }
     }
 }
