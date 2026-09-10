2e482c331492dd3e7794cc3926c2c574d735ad07
PRERNA PREM <prernaprem720@gmail.com>
2026-09-10 13:42:21 +0530

    Time: 0 ms (100%), Space: 47.3 MB (65.87%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -2,10 +2,11 @@ class Solution {
     public int missingNumber(int[] nums) {
         int n = nums.length;
         int sum = n * (n + 1) / 2;// find the n natural number sum
+        int sum1 = 0;
         for(int x : nums){
-            sum -= x;
+            sum1 += x;
         }
-        return sum;
+        return (sum - sum1);
 
     }
 }
