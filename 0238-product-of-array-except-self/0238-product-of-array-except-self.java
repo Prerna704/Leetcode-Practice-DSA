e492b98a0786ceebbde0bc4a2613b39c4b53d870
PRERNA PREM <prernaprem720@gmail.com>
2026-09-10 13:45:32 +0530

    Time: 2 ms (93.2%), Space: 72.2 MB (29.55%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -3,16 +3,14 @@ class Solution {
         int pref = 1;
         int suff =1;
         int[] res = new int[nums.length];
-         for(int i =0; i <nums.length; i++){
+       for(int i = 0; i < nums.length; i++){
             res[i] = pref;
             pref *= nums[i];
-
-        }
-
-        for(int i = nums.length - 1; i>=0; i--){
-           res[i] *= suff;
-           suff *= nums[i];
-        }
+       }
+       for(int i = nums.length - 1; i>= 0; i--){
+            res[i] *= suff;
+            suff *= nums[i];
+       }
 
        
         return res;
