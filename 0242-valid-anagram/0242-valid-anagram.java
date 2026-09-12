3abe51762a315b8648a224d2dcaff53d609e0dd5
PRERNA PREM <prernaprem720@gmail.com>
2026-09-12 17:32:05 +0530

    Time: 4 ms (83.72%), Space: 44.3 MB (82.01%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -3,17 +3,15 @@ class Solution {
        int n = s.length();
        int m = t.length();
         int[] count = new int[26];
-       if(n != m)return false;
-
-       for(int i = 0; i < n; i++){
-            count[s.charAt(i) - 'a']++;
-            count[t.charAt(i) - 'a']--;
-       }
-       for(int x : count){
-        if(x != 0)return false;
-       }
-        return true;
-
+        if(n != m) return false;
+      for(int i = 0;i < n; i++){
+        count[s.charAt(i) -'a']++;
+        count[t.charAt(i) - 'a']--;
+      }
+      for(int i = 0; i < count.length; i++){
+        if(count[i] != 0)return false;
+      }
+      return true;
 
     }
 }
