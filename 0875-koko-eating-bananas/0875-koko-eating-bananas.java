3a1c2110735e72148dc49bd3c91b0329fecd7410
PRERNA PREM <prernaprem720@gmail.com>
2026-09-17 09:30:30 +0530

    Time: 7 ms (83.68%), Space: 48.3 MB (18.67%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.java
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.java
@@ -1,27 +1,33 @@
 class Solution {
-    public int minEatingSpeed(int[] piles, int hour) {
-        int n=piles.length;
-        int ans=0;
-        int l=1;
-        int h=piles[0];
-        // choose max for h
-        for(int pile:piles){
-            h=Math.max(h,pile);
+    public int minEatingSpeed(int[] piles, int h) {
+       int left = 1;
+    int right = 0;
+    
+    // Find the maximum pile size to set the upper bound for binary search
+    for (int pile : piles) {
+        if (pile > right) {
+            right = pile;
         }
-
-        while(l<=h){
-            int mid=l+(h-l)/2;
-            long maxhours=0;
-            for(int num:piles){
-                maxhours+=(long)(num+mid-1)/mid;
-            }
-            if(maxhours<=hour){
-                ans=mid;
-                h=mid-1;
-            }else{
-                l=mid+1;
-            }
+    }
+    
+    // Binary search for the minimum valid speed k
+    while (left < right) {
+        int mid = left + (right - left) / 2;
+        long totalHours = 0;
+        
+        // Calculate total hours needed with the current eating speed (mid)
+        for (int pile : piles) {
+            totalHours += (pile + mid - 1) / mid; // Equivalent to Math.ceil((double)pile / mid)
         }
-        return ans;
+        
+        // If Koko can finish within h hours, try a slower speed
+        if (totalHours <= h) {
+            right = mid;
+        } else { // Otherwise, Koko needs to eat faster
+            left = mid + 1;
+        }
+    }
+    
+    return left;
     }
 }
