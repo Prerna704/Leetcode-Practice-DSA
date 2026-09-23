80115f13085b662b3ba82e58bdc1ebbfee7f7b19
PRERNA PREM <prernaprem720@gmail.com>
2026-09-23 10:15:35 +0530

    Time: 6 ms (80.8%), Space: 44.4 MB (46.62%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
@@ -1,10 +1,12 @@
 class Solution {
     public String reverseWords(String s) {
-        String[] str =s.trim().split("\\s+");
+        String[] arr = s.trim().split("\\s+");
         StringBuilder sb = new StringBuilder();
-        for(int i = str.length - 1;i >=0; i--){
-            sb.append(str[i]).append(" ");
+
+        for(int i = arr.length - 1; i >= 0; i--){
+            sb.append(arr[i]).append(" ");
         }
         return sb.toString().trim();
+
     }
 }
