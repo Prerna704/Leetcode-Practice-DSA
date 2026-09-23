b54c86dea248017cf8902a642af5d08b36fa9cd5
PRERNA PREM <prernaprem720@gmail.com>
2026-09-23 10:31:52 +0530

    Time: 3 ms (56.06%), Space: 44 MB (95.72%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,27 +1,30 @@
 class Solution {
     public boolean isPalindrome(String s) {
-       int l = 0;
-       int r = s.length() - 1;
-       String str = s.toLowerCase();
-       while(l < r){
-        //for left
-            while(l < r && !Character.isLetterOrDigit(str.charAt(l))){
-                l++;
-            }
-            //for right
-            while(l < r && !Character.isLetterOrDigit(str.charAt(r))){
-                r--;
-            }
-            //check left or right cahracter
-            if(str.charAt(l)!=str.charAt(r)){
+      int l = 0;
+      int r = s.length() - 1;
+      String str = s.toLowerCase();
+      while(l < r){
+
+        while(l < r && !Character.isLetterOrDigit(str.charAt(l))){
+            l++;
+        }
+
+        while(l < r && !Character.isLetterOrDigit(str.charAt(r))){
+            r--;
+        }
+
+
+            if(str.charAt(l) != str.charAt(r)){
                 return false;
-            }
-            else{
+            }else{
                 l++;
                 r--;
             }
 
-       }
-       return true;
+
+
+      }
+
+      return true;
     }
 }
