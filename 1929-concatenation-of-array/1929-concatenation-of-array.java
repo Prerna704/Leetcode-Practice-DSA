1979077fdc63f7bd452a08c0e1e8139b3a777df2
PRERNA PREM <prernaprem720@gmail.com>
2026-09-24 21:32:02 +0530

    Time: 1 ms (97.57%), Space: 47.2 MB (56.08%) - LeetHub

--- 1929-concatenation-of-array/1929-concatenation-of-array.java
+++ 1929-concatenation-of-array/1929-concatenation-of-array.java
@@ -1,12 +1,14 @@
 class Solution {
     public int[] getConcatenation(int[] nums) {
+       
+
         int n = nums.length;
         int m = 2*n;
-        int[] ans = new int[m];
+        int[] res = new int[m];
         for(int i = 0; i < n; i++){
-            ans[i] = nums[i];
-            ans[i + n] = nums[i];
+            res[i] = nums[i];
+            res[i + n] = nums[i];
         }
-        return ans;
+        return res;
     }
 }
