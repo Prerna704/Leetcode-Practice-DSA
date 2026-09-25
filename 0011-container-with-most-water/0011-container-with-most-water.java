d8aa15879713a43c71b006dc5a14bbe9f59317b1
PRERNA PREM <prernaprem720@gmail.com>
2026-09-25 18:44:00 +0530

    Time: 5 ms (82.72%), Space: 77.5 MB (29.62%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.java
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -1,21 +1,19 @@
 class Solution {
-    public int maxArea(int[] height) {
-       int l = 0; 
-       int r = height.length - 1;
-       int mxarea = 0;
-       while(l < r){
-            int w = r - l;
-            int h = Math.min(height[l],height[r]);
-            int area = w * h;
-            mxarea = Math.max(area,mxarea);
+    public int maxArea(int[] arr) {
+      int l = 0; int r = arr.length -1;int maxarea = 0;
 
-            if(height[l] < height[r]){
+        while(l < r){
+            int w = r - l;
+            int h =Math.min(arr[l] , arr[r]);
+            int area = w  * h;
+            maxarea = Math.max(area,maxarea);
+
+            if(arr[l] < arr[r]){
                 l++;
             }else{
                 r--;
             }
-
-       }
-       return mxarea;
+        }
+        return maxarea;
     }
 }
